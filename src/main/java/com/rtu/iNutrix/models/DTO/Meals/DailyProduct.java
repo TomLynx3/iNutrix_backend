841e17b70dac7268b21c9719990864a9cc722bab
Artur <andrijanov82@gmail.com>
2022-12-04 18:25:24 +0200

    Done, not tested

--- src/main/java/com/rtu/iNutrix/models/DTO/Meals/DailyProduct.java
+++ src/main/java/com/rtu/iNutrix/models/DTO/Meals/DailyProduct.java
@@ -23,8 +23,11 @@ public class DailyProduct {
 
     private ProductGroupDTO productGroup;
 
+    private ProductDTO productDTO;
 
-    public DailyProduct(ProductDTO product,double amount){
+
+    public DailyProduct(ProductDTO product, double amount){
+        this.productDTO = product;
         this.productId = product.getId();
         this.name = product.getName();
         this.amount = amount;
